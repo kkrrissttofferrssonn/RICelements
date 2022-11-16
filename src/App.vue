f579356a6120c3a49f9650e7bb527bb6dfc4e656
kkrrissttofferrssonn <christopheralduncan@gmail.com>
2022-11-16 14:55:37 -0500

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+  <h1>Create a Flex Container</h1>
+
+  <div class="flex-container">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>3</div>
+    <div>3</div>
+    <div>3</div>
+    <div>3</div>
+  </div>
+
+  <p>
+    A Flexible Layout must have a parent element with the
+    <em>display</em> property set to <em>flex</em>.
+  </p>
+
+  <p>
+    Direct child elements(s) of the flexible container automatically becomes
+    flexible items.
+  </p>
+  <div class="flex-container">
+    <div style="flex-grow: 1">1</div>
+    <div style="flex-grow: 1">2</div>
+    <div style="flex-grow: 8">3</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +50,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.flex-container {
+  display: flex;
+  background-color: DodgerBlue;
+}
+
+.flex-container > div {
+  background-color: #f1f1f1;
+  margin: 10px;
+  padding: 20px;
+  font-size: 30px;
 }
 </style>
